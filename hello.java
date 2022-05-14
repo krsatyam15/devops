f94b1827683a500c0793d8191559bfc531fe01e9
kumar satyam <mishusatyam@gmail.com>
2022-05-14 16:08:10 +0530

    my third commit

--- hello.java
+++ hello.java
@@ -3,3 +3,8 @@ class Hello{
     System.out.println("This is java app \n by using Docker");  
     }  
     } 
+
+ public static void main(String[] args){  
+    System.out.println("This is java app v2  \n by using Docker");  
+    }
+    }
